35c6730134ee9cbf94f7d5108434dfcae3b5eb07
shreyanshdokania <shreyanshdokania@gmail.com>
2021-09-08 16:42:16 +0530

    notdeprecated

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MediaQuery(
+      data: new MediaQueryData(),
+      child: new MaterialApp(
+        home: HomePage(),
+      ),
+    );
+  }
+}
